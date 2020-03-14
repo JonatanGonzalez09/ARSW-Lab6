964f198855bf2f4cddc33328ee0316a9974a824a
JonatanGonzalez09 <jonatan0924@gmail.com>
2020-03-14 18:06:58 -0500

    Dibuja puntos en el canvas y actualiza autores

--- BLUEPRINTS_PART1/src/main/resources/static/js/apiclient.js
+++ BLUEPRINTS_PART1/src/main/resources/static/js/apiclient.js
@@ -1,6 +1,11 @@
+<<<<<<< HEAD
+var apiUrl = "http://localhost:8080/blueprints/"
+apiclient = (function() {
+=======
 const apiUrl = "http://localhost:8080/blueprints/"
 apiclient = (function() {
 
+>>>>>>> a26f70d59dd229f8bee7be020a7d8904d23b079d
     return {
         getBlueprintsByAuthor: function(name, callback) {
             jQuery.ajax({
@@ -19,6 +24,45 @@ apiclient = (function() {
                 },
                 async: true
             });
+<<<<<<< HEAD
+        }, 
+        setBlueprint: function(author, name, newPlan) {
+          
+            var putPromise = $.ajax({
+              url: "/blueprints/" + author + "/" + name + "/",
+              type: "PUT",
+              data: newPlan,
+              contentType: "application/json"
+            });
+            putPromise.then(
+              function() {
+                console.log(putPromise);
+                app.getBlueprintsByAuthor(author);
+              },
+              function() {
+                console.info("Error");
+              }
+            );
+          },
+          deleteBlueprint: function(author, name) {
+          
+            var putPromise = $.ajax({
+              url: "/blueprints/" + author + "/" + name + "/",
+              type: "DELETE",
+              contentType: "application/json"
+            });
+            putPromise.then(
+              function() {
+                console.log(putPromise);
+                app.getBlueprintsByAuthor(author);
+              },
+              function() {
+                console.info("Error");
+              }
+            );
+          }
+=======
         }
+>>>>>>> a26f70d59dd229f8bee7be020a7d8904d23b079d
     };
 })();
